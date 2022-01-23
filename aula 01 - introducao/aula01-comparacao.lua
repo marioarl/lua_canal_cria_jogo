341eff011780b28126bfef20028eb3a2595950eb
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-23 16:46:03 -0300

    Criacao aula01-comparacao.lua

---
+++ aula 01 - introducao/aula01-comparacao.lua
@@ -0,0 +1,20 @@
+--[[
+    Operadores de comparacao retornam um valor BOOLEAN (true, false)
+    Maior que       >
+    Menor que       <
+    Maior ou igual  >=
+    Menor ou igual  <=
+    Igualdade       ==
+]]
+
+print(2 > 3)
+--O retorno deste comando sera FALSE
+
+print(2 < 3)
+--O retorno deste comando será TRUE
+
+print(2 <= 3)
+--O retorno deste comando será TRUE
+
+print(2 >= 3)
+--O retorno deste comando sera FALSE
