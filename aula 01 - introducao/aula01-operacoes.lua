672dc45ecf1eceba0ba17dfa5c09f19c7c2b8284
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-23 16:51:20 -0300

    Criacao aula01-operacoes.lua

---
+++ aula 01 - introducao/aula01-operacoes.lua
@@ -0,0 +1,16 @@
+--[[
+    operacoes matematicas
+    Soma                +
+    Subtracao           -
+    Multiplicacao       *
+    Divisao             /
+    Resto da divisao    %
+
+
+]]
+
+print(1 + 1)
+print(2 -1)
+print(3 * 3)
+print(4 / 2)
+print(9%2)
