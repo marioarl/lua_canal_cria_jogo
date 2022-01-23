9c720d635ba4ec07dbc7aa5f10a4d9b7974c2b76
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-23 16:53:35 -0300

    Criacao aula01.lua

---
+++ aula 01 - introducao/aula01.lua
@@ -0,0 +1,10 @@
+print("Ola Mundo") --Para exibir o texto, quando tiver letras, é necessario colocar entre " "
+print(123456) --Para exibir numeros não é necessario coloca-los entre " "
+
+
+-- Esta linha é um comentario simples
+
+--[[
+        COMENTARIO EM BLOCO
+
+]]
