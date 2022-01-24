a94466daf7d5b1df13717b028f2724d2cb77d63a
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-24 16:29:44 -0300

    Criacao aula02-concatenar.lua

---
+++ aula 02 - variaveis strings e conc de texto/aula02-concatenar.lua
@@ -0,0 +1,8 @@
+linha1 = "Primeira Linha"
+linha2 = "Segunda Linha"
+
+print(linha1 .. " ".. "e".." "..linha2) --O operador .. é a concatenacao
+
+titulo = "Voce me deve: "
+valor = 450
+print(titulo .. "R$"..valor)
