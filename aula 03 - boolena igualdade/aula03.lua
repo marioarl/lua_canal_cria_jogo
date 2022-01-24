f13221f3c3096f1855b908aba76a31bd27346e56
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-24 16:32:10 -0300

    Criacao aula03.lua

---
+++ aula 03 - boolena igualdade/aula03.lua
@@ -0,0 +1,7 @@
+--[[boolean só pode assumir dois possiveis valores:
+true ou false
+]]--
+
+estaAtivo = false
+
+print("Valor de estaAtivo: " .. tostring (estaAtivo)) -- para imprimir um texto e uma variavel temos que utilizar tostring(), que converterá o valor da variavel para uma string.
