ddaa5f7957793a73fe81723ea9ab169d94ef3b54
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-24 16:30:10 -0300

    Criacao aula02-variaveis.lua

---
+++ aula 02 - variaveis strings e conc de texto/aula02-variaveis.lua
@@ -0,0 +1,13 @@
+msg = "Ola Mundo" --Criar uma variavel em LUA colocar o nome da variavel e o valor da variavel
+
+--[[
+    1)Nomes de variaveis nao podem ter acentos ou caracteres especiais
+    2)Podem ter Letras, numeros e Underscore
+    3)Nomes de variaveis nao podem ter espacos
+    4)Nomes de variaveis nao podem ser iguais a palavras reservadas pela linguagem (ex. false, true, print, if, then, else, end...)
+
+    CASE SENSITIVE
+]]
+
+print(msg) --Imprimindo a variavel
+
