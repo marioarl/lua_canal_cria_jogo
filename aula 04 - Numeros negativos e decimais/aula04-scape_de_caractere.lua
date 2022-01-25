a10e2c6a178d139ed4905711ce64b3bebc18e306
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-25 16:58:22 -0300

    Criacao aula04-scape

---
+++ aula 04 - Numeros negativos e decimais/aula04-scape_de_caractere.lua
@@ -0,0 +1,33 @@
+--[[CARACTERES DE ESCAPE
+\'          ==> Aspa simples
+\"          ==> Aspa Dupla
+\\          ==> Barra invertida
+\n          ==> Pular uma linha
+\t          ==> Tabulacao horizontal
+\v          ==> Taulacao vertical
+\r          ==> Retorno
+\b          ==> BackSpace
+\a          ==> Campainha
+
+
+]]
+
+
+
+nome = "Caixa D\'agua"
+print(nome)
+
+ironia = "Voce é \"rico\""
+print(ironia)
+
+barra = "Texto com \\"
+print(barra)
+
+variasLinhas = "Linha 1\nlinha 2\nLinha 3"
+print(variasLinhas)
+
+tabulacao = "\tTabulacao na esquerda"
+print(tabulacao)
+
+retorno = "\tA tabulacao esta aqui\n\re retornou aqui"
+print(retorno)
