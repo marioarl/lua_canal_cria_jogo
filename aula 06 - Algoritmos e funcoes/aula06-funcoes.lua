87d52702d99fb67964b66ece72e6960feabebd91
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-25 17:01:42 -0300

    Criacao aula06.lua

---
+++ aula 06 - Algoritmos e funcoes/aula06-funcoes.lua
@@ -0,0 +1,17 @@
+function somarUm() --Definção ou assinatura da função
+    print(1+1) -- Implementação da função
+end 
+
+somarUm()
+
+--Somar dois numeros passados pelo usuario
+
+function somarDoisNumeros(a, b) -- a e b sao parametros
+    print (a + b)
+end
+
+somarDoisNumeros(2, 2)
+somarDoisNumeros(10, 5)
+somarDoisNumeros(20,8)
+somarDoisNumeros(2, -2)
+somarDoisNumeros(2.5, 2.5)
---
+++ aula 06 - Algoritmos e funcoes/aula06.lua
@@ -0,0 +1,31 @@
+--[[ ALGORITMO
+É uma sequencia de comandos em uma ordem logica e sequencial a fim de realizar um objetivo pré definido.
+
+Ex. Jogar papel no lixo
+
+1 - Pegar o papel
+2 - Amassar o papel
+3 - Encontrar a lixeira
+4 - Jogar o papel na lixeira
+
+Ex. escovar os dentes
+
+1 - Ir ao banheiro
+2 - Abrir o armario
+3 - Pegar a escova
+4 - Pegar a bisnaga com pasta de dente
+5 - Abrir a bisnaga
+6 - Colocar a pasta na escova
+7 - Fechar a bisnaga
+8 - Molhar a escova
+9 - Mover a escova para a boca
+10 - Escovar os dentes
+11 - Cuspir na pia
+12 - Colocar a escova na pia
+13 - Abrir a torneira
+14 - Pegar agua
+15 - Enxaguar a boca
+16 - Cuspir na pia
+17 - Lava a escova
+18 - Guardar a escova
+]]--
