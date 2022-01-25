cb6899737abadab80d3b843e3468677aa49c35e2
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-25 16:57:41 -0300

    Criacao aula04-negativos.lua

---
+++ aula 04 - Numeros negativos e decimais/aula04-negativos.lua
@@ -0,0 +1,9 @@
+resultado = 4 - 10 --A Lua reconhece os numeros negativos
+print(resultado)
+
+numeroReal = 45.5 --Numeros Reais
+print(numeroReal )
+
+pi = 3.141592737373733849384081 -- Numeros Reais com muitas casas 
+print("PI "..pi)
+
