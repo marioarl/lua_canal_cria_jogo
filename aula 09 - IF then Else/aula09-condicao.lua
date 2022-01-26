07f2668dc9bbf2dc47c22c3b2a5dca3179874f50
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-26 15:51:20 -0300

    Criacao aula09-condicao.lua

---
+++ aula 09 - IF then Else/aula09-condicao.lua
@@ -0,0 +1,31 @@
+--[[
+
+if TESTE LOGICO then
+    bloco de comandos
+
+end
+
+]]
+
+
+
+print("Digite o 1o. valor: ")
+valor1 = io.read()
+
+print("Digite o 2o. valor: ")
+valor2 = io.read()
+
+if valor1 > valor2 then
+    print("Valor 1 e maior que o valor 2")
+end
+
+if valor1 < valor2 then
+    print("Valor 1 e menor que o valor 2")
+end
+
+if valor1 == valor2 then
+    print("Valor 1 e igual ao valor 2")
+end
+
+
+  
