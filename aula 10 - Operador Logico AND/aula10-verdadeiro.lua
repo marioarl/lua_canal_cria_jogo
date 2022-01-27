b0171347455c3b429fbdc86ab691562316af2978
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-27 16:11:43 -0300

    Criacao aula10-verdadeiro.lua

---
+++ aula 10 - Operador Logico AND/aula10-verdadeiro.lua
@@ -0,0 +1,19 @@
+--[[
+Tudo o que o IF espera é apenas true ou false
+
+Se true ==> executa o codigo dentro do bloco
+Se false ==> Cai para o proximo bloco: elseif ou else
+
+
+]]
+
+--[[
+ 
+
+]]
+
+if true ~= false then
+    print("verdadeiro")
+else
+    print("Falso")
+end 
