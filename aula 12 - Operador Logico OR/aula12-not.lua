410cc527a0f11cecef3fd912c24faccebc0bdbba
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-31 11:02:19 -0300

    Criacao aula12-not.lua

---
+++ aula 12 - Operador Logico OR/aula12-not.lua
@@ -0,0 +1,16 @@
+--[[
+O operador NOT inverte o valor seguinte.
+
+* Se o valor for falso, ele se torna verdadeiro
+* se o valor for verdadeiro, ele se torna falso 
+
+]]
+
+if not false then
+    print("not false")
+end
+
+
+if not true then
+    print("not true")
+end
